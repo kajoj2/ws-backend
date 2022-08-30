779d4489ec56a675badc9febfc0495b3337f12d1
Karol <karolprzybylak1@gmail.com>
2022-08-30 21:38:13 +0200

    init

---
+++ Sensor/Sensor/Controller/SensorController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Sensor.Sensor.Controller;
+
+[ApiController]
+[Route("[controller]")]
+public class SensorController
+{
+
+    [HttpGet]
+    public IActionResult Sensor()
+    {
+
+        return new OkObjectResult("Sensor");
+    }
+    
+}
